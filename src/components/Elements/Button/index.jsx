5f2115a48e206e44b9c8b33ee2cd4d30d5d1c213
PramudyaReynaldi <prmdy02@gmail.com>
2023-07-12 14:54:27 +0700

    add props

--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 const Button = (props) => {
-  const { style, children } = props;
+  const { style, children, type, onClick } = props;
   return (
     // Menggunakan props untuk mengubah masing-masing warna background dan teks pada button
     <button
       className={`h-10 px-6 font-semibold rounded-md ${style} text-white`}
-      type="submit"
+      type={type}
+      onClick={onClick}
     >
       {/* props.children => untuk merender tanpa harus menggunakan variabel seperti "variant" */}
       {children}
