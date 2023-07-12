f77c6947407b8b64dc43fa9eb8385c9ecdc284d4
PramudyaReynaldi <prmdy02@gmail.com>
2023-07-12 14:54:51 +0700

    add localStorage

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,6 +2,7 @@
 import CardProduct from "../components/Fragments/CardProduct";
 import imgJordan from "../assets/fashion-shoes-sneakers.jpg";
 import imgSneakers from "../assets/sneakers.jpg";
+import Button from "../components/Elements/Button";
 
 const products = [
   {
@@ -14,26 +15,42 @@ const products = [
     cupiditate, totam cumque.`,
   },
   {
-    id: 1,
+    id: 2,
     name: "Jordan",
     price: "800.000",
     image: imgJordan,
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
-  }
+  },
 ];
 
+const email = localStorage.getItem("email");
+
 export default function ProductsPage() {
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.href = "/";
+  };
+
   return (
-    <div className="flex justify-center py-5">
-      {products.map((product) => (
-        <CardProduct>
-          <CardProduct.CardHeader image={product.image} />
-          <CardProduct.CardBody name={product.name}>
-            {product.description}
-          </CardProduct.CardBody>
-          <CardProduct.CardFooter price={`Rp. ${product.price}`} />
-        </CardProduct>
-      ))}
-    </div>
+    <>
+      <div className="justify-end flex h-10 bg-blue-600 text-white items-center px-10">
+        <h1>{email}</h1>
+        <Button style="bg-black ml-5" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+      <div className="flex justify-center py-5">
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.CardHeader image={product.image} />
+            <CardProduct.CardBody name={product.name}>
+              {product.description}
+            </CardProduct.CardBody>
+            <CardProduct.CardFooter price={`Rp. ${product.price}`} />
+          </CardProduct>
+        ))}
+      </div>
+    </>
   );
 }
