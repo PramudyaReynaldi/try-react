f6b3aac95b282d3febf8f4543e5c8be8bcbdb4ea
PramudyaReynaldi <prmdy02@gmail.com>
2023-07-12 14:54:38 +0700

    add logic login

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -2,9 +2,26 @@ import InputForm from "../Elements/Input";
 import Button from "../Elements/Button";
 
 export default function FormLogin() {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    localStorage.setItem("email", e.target.email.value);
+    localStorage.setItem("password", e.target.password.value);
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    if (email !== "" && password !== "") {
+      alert("Login success")
+      window.location.href = "/products"
+    } else {
+      alert("Please enter a valid email and password")
+      window.location.reload()
+    }
+  };
+
   return (
     <>
-      <form action="">
+      <form onSubmit={handleLogin}>
         <InputForm
           label="Email"
           type="email"
@@ -17,7 +34,9 @@ export default function FormLogin() {
           placeholder="*****"
           name="password"
         />
-        <Button style="bg-blue-600 w-full">Login</Button>
+        <Button style="bg-blue-600 w-full" type="submit">
+          Login
+        </Button>
       </form>
     </>
   );
